3be798ade559f312c9cb6552e25b40cafcc9568f
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-09 13:42:36 +0800

    fetching data and inserting

--- Session8_090120/connecttodbExercise.py
+++ Session8_090120/connecttodbExercise.py
@@ -7,6 +7,7 @@ testConnect = psycopg2.connect(user="postgres",
     host="127.0.0.1",
     port="5432",
     database="testDB")
+testConnect.autocommit = True
 #(referring to .cursor)Cursor class is an instance using which you can invoke methods that execute SQLite statements, 
 # fetch data from the result sets of the queries. 
 # You can create Cursor object using the cursor() method of the Connection object/class.
@@ -23,10 +24,19 @@ cursor = testConnect.cursor()
 #insert_query = """ INSERT INTO Userdata (ID, users, password) VALUES (1, 'Abhisek Rai', '1234')"""
 #cursor.execute(insert_query)
 
-update_query = """ UPDATE Userdata SET ID = '2' WHERE ID = '1'"""
+update_query = """ UPDATE Userdata SET ID = '1' WHERE ID = '2'"""
 cursor.execute(update_query)
 testConnect.commit()
 print("1 Record updated successfully")
+
+insert_query = """ INSERT INTO Userdata (ID, users, password) VALUES (2, 'Natasha Ho', '5678')"""
+cursor.execute(insert_query)
+
+#FETCH RESULT
+select_query = "select * from Userdata"
+cursor.execute(select_query)
+data_fetch_from_db = cursor.fetchall()
+print("data from table", data_fetch_from_db)
 
 
 if (testConnect):
