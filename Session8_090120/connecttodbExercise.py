3c5a9dc37b24ee18b931acefec0d17d4fd6f3568
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-09 12:27:29 +0800

    updating data in sql

--- Session8_090120/connecttodbExercise.py
+++ Session8_090120/connecttodbExercise.py
@@ -7,7 +7,7 @@ testConnect = psycopg2.connect(user="postgres",
     host="127.0.0.1",
     port="5432",
     database="testDB")
-#Cursor class is an instance using which you can invoke methods that execute SQLite statements, 
+#(referring to .cursor)Cursor class is an instance using which you can invoke methods that execute SQLite statements, 
 # fetch data from the result sets of the queries. 
 # You can create Cursor object using the cursor() method of the Connection object/class.
 cursor = testConnect.cursor()
@@ -19,6 +19,12 @@ create_table_query = '''CREATE TABLE Userdata
 # Execute a command: this creates a new table
 cursor.execute(create_table_query)
 testConnect.commit()
+
+insert_query = """ INSERT INTO Userdata (ID, users, password) VALUES (1, 'Abhisek Rai', '1234')"""
+cursor.execute(insert_query)
+testConnect.commit()
+print("1 Record inserted successfully")
+
 
 if (testConnect):
     print("Connected to Database")    
